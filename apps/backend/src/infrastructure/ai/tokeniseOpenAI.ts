76493298dcf4a200c2f696dd80a66b9e923f295a
TCotton <me@andywalpole.me>
2025-12-07 20:28:11 +0000

    fix: formatted tokeniseOpenAI function

--- apps/backend/src/infrastructure/ai/tokeniseOpenAI.ts
+++ apps/backend/src/infrastructure/ai/tokeniseOpenAI.ts
@@ -1,24 +1,18 @@
-import { Tiktoken } from 'js-tiktoken/lite';
-import o200k_base from 'js-tiktoken/ranks/o200k_base';
+import { Tiktoken } from 'js-tiktoken/lite'
+import o200k_base from 'js-tiktoken/ranks/o200k_base'
 import { readFileSync } from 'node:fs'
 import path from 'node:path'
 
 const tokeniseOpenAI = (file: string) => {
-
-  const tokenizer = new Tiktoken(
-    o200k_base,
-  );
+  const tokenizer = new Tiktoken(o200k_base)
 
   const tokenize = (text: string) => {
-    return tokenizer.encode(text);
-  };
+    return tokenizer.encode(text)
+  }
 
-  const input = readFileSync(
-    path.join(import.meta.dirname, file),
-    'utf-8',
-  );
+  const input = readFileSync(path.join(import.meta.dirname, file), 'utf-8')
 
-  return tokenize(input);
+  return tokenize(input)
 }
 
 export { tokeniseOpenAI }
